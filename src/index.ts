c40d215a9cb61e66c8f136bab2f3956c740fbb28
retueZe <m.redcoder@gmail.com>
2021-04-28 21:10:29 +0300

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { execSync } from 'child_process';
-import { existsSync, mkdirSync, rmSync } from 'fs';
+import { existsSync, mkdirSync, rmSync, readdirSync, readFileSync } from 'fs';
 import path from 'path';
 
 export default function clearCache(): void {
     const npmCacheDirectory = execSync('npm config get cache').toString().trimRight();
     const npxCacheDirectory = path.join(npmCacheDirectory, '_npx');
 
-    if (existsSync(npxCacheDirectory)) rmSync(npxCacheDirectory, {recursive: true});
+    if (existsSync(npxCacheDirectory)) {
+        for (const subdirectoryName of readdirSync(npxCacheDirectory)) {
+            if (subdirectoryName === '066c52483104548d') continue;
 
-    mkdirSync(npxCacheDirectory);
+            const subdirectory = path.join(npxCacheDirectory, subdirectoryName);
+            const package_json = JSON.parse(readFileSync(path.join(subdirectory, 'package.json')).toString());
+            
+            if (package_json.name === 'clear-npx-cache') continue;
+
+            rmSync(subdirectory, {recursive: true});
+        }
+    }
+    else
+        mkdirSync(npxCacheDirectory);
 }
