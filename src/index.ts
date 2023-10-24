473f9da3beff9bc36363bfffa85c0749d1c19b1f
gabriel-logan <gabrielcoutolima@protonmail.ch>
2023-10-23 22:50:14 -0300

    I added a bar graph, changed trim Right to trimEnd and added some logs to inform users of the progress

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,48 @@
 import { execSync } from 'child_process';
 import { existsSync, mkdirSync, rmSync, readdirSync, readFileSync } from 'fs';
 import path from 'path';
+import cliProgress from 'cli-progress';
 
 export default function clearCache(): void {
-    const npmCacheDirectory = execSync('npm config get cache').toString().trimRight();
-    const npxCacheDirectory = path.join(npmCacheDirectory, '_npx');
+  const npmCacheDirectory = execSync('npm config get cache').toString().trimEnd();
+  const npxCacheDirectory = path.join(npmCacheDirectory, '_npx');
 
-    if (existsSync(npxCacheDirectory)) {
-        for (const subdirectoryName of readdirSync(npxCacheDirectory)) {
-            const subdirectory = path.join(npxCacheDirectory, subdirectoryName);
-            const packagePath = path.join(subdirectory, 'package.json');
+  // create a new progress bar instance and use shades_classic theme
+  const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-            if (existsSync(packagePath)) {
-                const package_json = JSON.parse(readFileSync(packagePath).toString());
-                
-                if (package_json.name === 'clear-npx-cache') continue;
-            }
+  console.log('Starting cleaning ... \n');
 
-            rmSync(subdirectory, {recursive: true});
-        }
+  if (existsSync(npxCacheDirectory)) {
+    const subdirectoryNames = readdirSync(npxCacheDirectory);
+    const subdirectories = subdirectoryNames.map((subdirectoryName) =>
+      path.join(npxCacheDirectory, subdirectoryName),
+    );
+
+    // start the progress bar with a total value of 200 and start value of 0
+    bar1.start(subdirectories.length, 0);
+
+    for (let i = 0; i < subdirectories.length; i += 1) {
+      const subdirectory = subdirectories[i];
+      const packagePath = path.join(subdirectory, 'package.json');
+
+      // update the current value in your application..
+      bar1.update(i + 1);
+
+      if (existsSync(packagePath)) {
+        const package_json = JSON.parse(readFileSync(packagePath).toString());
+
+        if (package_json.name === 'clear-npx-cache') continue;
+      }
+
+      rmSync(subdirectory, { recursive: true });
     }
-    else
-        mkdirSync(npxCacheDirectory);
+
+    // stop the progress bar
+    bar1.stop();
+
+    console.log('\n Cache cleared successfully! Thanks for using our lib.');
+  } else {
+    mkdirSync(npxCacheDirectory);
+    console.log('\n Everything up to date, cache already clean!');
+  }
 }
