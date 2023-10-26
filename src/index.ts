a569670c1bd730999524e1634a554d72962a17c3
retueZe <84510709+retueZe@users.noreply.github.com>
2023-10-26 16:32:12 +0300

    Fixed indentation and removed comments

--- src/index.ts
+++ src/index.ts
@@ -4,45 +4,42 @@ import path from 'path';
 import cliProgress from 'cli-progress';
 
 export default function clearCache(): void {
-  const npmCacheDirectory = execSync('npm config get cache').toString().trimEnd();
-  const npxCacheDirectory = path.join(npmCacheDirectory, '_npx');
+    const npmCacheDirectory = execSync('npm config get cache').toString().trimEnd();
+    const npxCacheDirectory = path.join(npmCacheDirectory, '_npx');
 
-  // create a new progress bar instance and use shades_classic theme
-  const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-  console.log('Starting cleaning ... \n');
+    console.log('Starting cleaning ... \n');
 
-  if (existsSync(npxCacheDirectory)) {
-    const subdirectoryNames = readdirSync(npxCacheDirectory);
-    const subdirectories = subdirectoryNames.map((subdirectoryName) =>
-      path.join(npxCacheDirectory, subdirectoryName),
-    );
+    if (existsSync(npxCacheDirectory)) {
+        const subdirectoryNames = readdirSync(npxCacheDirectory);
+        const subdirectories = subdirectoryNames.map((subdirectoryName) =>
+        path.join(npxCacheDirectory, subdirectoryName),
+        );
 
-    // start the progress bar with a total value of 200 and start value of 0
-    bar1.start(subdirectories.length, 0);
+        bar1.start(subdirectories.length, 0);
 
-    for (let i = 0; i < subdirectories.length; i += 1) {
-      const subdirectory = subdirectories[i];
-      const packagePath = path.join(subdirectory, 'package.json');
+        for (let i = 0; i < subdirectories.length; i += 1) {
+        const subdirectory = subdirectories[i];
+        const packagePath = path.join(subdirectory, 'package.json');
 
-      // update the current value in your application..
-      bar1.update(i + 1);
+        bar1.update(i + 1);
 
-      if (existsSync(packagePath)) {
-        const package_json = JSON.parse(readFileSync(packagePath).toString());
+        if (existsSync(packagePath)) {
+            const package_json = JSON.parse(readFileSync(packagePath).toString());
 
-        if (package_json.name === 'clear-npx-cache') continue;
-      }
+            if (package_json.name === 'clear-npx-cache') continue;
+        }
 
-      rmSync(subdirectory, { recursive: true });
+        rmSync(subdirectory, { recursive: true });
+        }
+
+        // stop the progress bar
+        bar1.stop();
+
+        console.log('\n Cache cleared successfully! Thanks for using our lib.');
+    } else {
+        mkdirSync(npxCacheDirectory);
+        console.log('\n Everything up to date, cache already clean!');
     }
-
-    // stop the progress bar
-    bar1.stop();
-
-    console.log('\n Cache cleared successfully! Thanks for using our lib.');
-  } else {
-    mkdirSync(npxCacheDirectory);
-    console.log('\n Everything up to date, cache already clean!');
-  }
 }
